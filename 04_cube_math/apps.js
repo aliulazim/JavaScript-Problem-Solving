c3d6920e870f639eb279695e531a76529f20be81
aliul azim <aliulazimbd@gmail.com>
2020-10-01 23:59:18 +0600

    04_cube_math

--- 04_cube_math/apps.js
+++ 04_cube_math/apps.js
@@ -9,8 +9,48 @@ console.log(output); // --> 27
 */
 
 
+// 1St way
+
 function cube(num) {
     return Math.pow(num, 3);  // Here 3 is Power
 }
 let output = cube(3);  //  3 cubed = 3 = 3 × 3 × 3 = 27
 console.log(output);
+
+
+// 2nd way
+
+/*
+function cube(num) {
+    return num * num * num;
+}
+
+let output = cube(3);
+console.log(output);
+
+*/
+
+
+
+// 3rd way
+
+/*
+    function cube() {
+    return 3 3 3;
+    }
+    
+    console.log(cube());
+*/
+
+
+// 4th way
+
+/*
+
+function cube(n) {
+    return  n ** 3;
+}
+let output = cube(3);
+console.log(output);
+
+*/
