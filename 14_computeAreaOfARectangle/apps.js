d916880b10c4f69f0c3e522b2aeb8e40461b00ea
aliul azim <aliulazimbd@gmail.com>
2020-10-02 00:23:22 +0600

    computeAreaOfARectangle

---
+++ 14_computeAreaOfARectangle/apps.js
@@ -0,0 +1,15 @@
+/*
+Write a function called “computeAreaOfARectangle”.
+
+Given the length and width of a rectangle, “computeAreaOfARectangle” returns its area.
+*/
+
+
+
+
+
+function computeAreaOfARectangle(length, width) {
+  return length * width;
+}
+let totalArea = computeAreaOfARectangle(10 , 5);
+console.log(totalArea);
