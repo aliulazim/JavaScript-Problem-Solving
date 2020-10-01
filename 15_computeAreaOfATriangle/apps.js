9d1b8438e06d3b084b2ab6535bf6d43fa7182448
aliul azim <aliulazimbd@gmail.com>
2020-10-02 00:33:40 +0600

    computeAreaOfATriangle

---
+++ 15_computeAreaOfATriangle/apps.js
@@ -0,0 +1,16 @@
+/*
+Write a function called “computeAreaOfATriangle”.
+
+Given the base and height of a triangle, “computeAreaOfATriangle” returns its area.
+
+*/
+
+
+
+
+
+function computeAreaOfATriangle(base, height) {
+  return (base * height) / 2;
+}
+let triangleArea = computeAreaOfATriangle(4, 5);
+console.log(triangleArea);
