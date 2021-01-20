25d80684e594bee02a93bab3500a5d27ed175478
aliulazim <aliulazimbd@gmail.com>
2021-01-20 20:48:41 +0600

    Js Rondom Number

---
+++ random.js
@@ -0,0 +1,10 @@
+let num = 2.489;
+let result = Math.floor(num);
+let result2 = Math.ceil(num);
+let result3 = Math.
+round(num);
+for (let i = 0; i < 10; i++) {
+    let dice = Math.random(num) * 10;
+    let output = Math.round(dice);
+    console.log(output);
+}
