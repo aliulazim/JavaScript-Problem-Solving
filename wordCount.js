dcc73fbe3d7d53c6f9935e039f87968ac297ecce
aliulazim <aliulazimbd@gmail.com>
2021-01-21 10:39:39 +0600

    count the charter in words

---
+++ wordCount.js
@@ -0,0 +1,14 @@
+let speech = "I am aliul     azim. I am a muslim. I love Allah and Rashul shalelahu walahe sallam.";
+
+let count = 0;
+
+for (let i = 0; i < speech.length; i++) {
+    const char = speech[i];
+
+    if (char == " " && speech[i - 1] != " ") {
+        count++;
+    }
+    
+}
+count++;
+console.log(count);
