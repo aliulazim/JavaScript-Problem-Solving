315d7ab20a62e3b653eb0b72e077ace0fc1021c1
aliulazim <aliulazimbd@gmail.com>
2021-01-24 19:48:26 +0600

    Inch to Feet

---
+++ inchToFeet.js
@@ -0,0 +1,14 @@
+// Convart inch to feet
+
+function inchToFeet(inch) {
+    var feet = inch / 12;
+    return feet;
+}
+var naniFeet = inchToFeet(132);
+console.log(naniFeet);
+var nanaFeet = inchToFeet(252);
+console.log(nanaFeet);
+var dadaFeet = inchToFeet(300);
+console.log(dadaFeet);
+var dadiFeet = inchToFeet(432);
+console.log(dadiFeet);
